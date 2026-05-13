51ec4bdaf76935acc17c1834373034473c5ad32f
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-14 00:20:50 +0530

    Section 3 basic react page with tailwinfd setup completed

---
+++ 02_Design_work/src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+      <>
+      <h1 className='text-blue-600 dark:text-green-500 border-2 p-4 rounded-xl'>Hello, React!</h1>
+      </>
+  )
+}
+
+export default App
