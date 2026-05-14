96d4f6487e3e6b45495a292b0beb0e2ae5d7bdaa
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-14 14:30:18 +0530

    added hero and header section in the app

--- 02_Design_work/src/App.jsx
+++ 02_Design_work/src/App.jsx
@@ -1,12 +1,16 @@
 import Card from "./components/Card"
-
+import Header from "./components/Header"
+import Hero from "./components/Hero"
 
 
 function App() {
 
   return (
+
     <div className="min-h-screen bg-black flex   justify-center 
     items-center flex-col overflow-hidden p-4">
+      <Hero />
+      <Header />
       <h1 className='text-blue-600 dark:text-green-500 border-2 p-4 rounded-xl'>Hello, React!</h1>
       <div className="flex gap-4 flex-wrap  mt-8">
         <Card
