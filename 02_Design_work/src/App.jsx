d14b28826f9df556df378f95a3a63282e084ed5a
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-15 16:43:49 +0530

    addedd shad cdn

--- 02_Design_work/src/App.jsx
+++ 02_Design_work/src/App.jsx
@@ -1,8 +1,7 @@
 import Card from "./components/Card"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
-
-
+import { Button } from "./components/ui/button.tsx"
 function App() {
 
   return (
@@ -10,6 +9,7 @@ function App() {
     <div className="min-h-screen bg-black flex   justify-center 
     items-center flex-col overflow-hidden p-4">
       <Hero />
+      <Button variant="outline" >Click Me</Button>
       <Header />
       <h1 className='text-blue-600 dark:text-green-500 border-2 p-4 rounded-xl'>Hello, React!</h1>
       <div className="flex gap-4 flex-wrap  mt-8">
