fd0f12f0326f68bc4fef98281d239959e43158cd
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-15 20:07:20 +0530

    Section 5 intiated the project

---
+++ 04_queue_management/src/App.jsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return(
+    <div className="app">
+      <header>
+        <h1>Queue Management App</h1>
+        <p>Mange your customers efficiently
+        </p>
+      </header>
+    </div>
+  )
+}
