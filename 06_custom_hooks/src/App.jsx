b517ad292680bfa0a020fd7b7818269ae540f351
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-17 19:46:44 +0530

    section 7 custom hooks project setup

---
+++ 06_custom_hooks/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className=""> </div>
+      <H1>Custom Hooks Class</H1>
+
+    </>
+
+  )
+}
+
+export default App
