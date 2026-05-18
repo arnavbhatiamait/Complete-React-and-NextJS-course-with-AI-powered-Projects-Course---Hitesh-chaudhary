e53a3bcad0088bb16359c980a27d75093e7be336
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-18 15:41:09 +0530

    changes in the variables in js

---
+++ JavaScript/part1/changes.js
@@ -0,0 +1,10 @@
+let gameName ="spiderman"
+gameName ="batman"
+console.log(gameName)
+
+const username="arnav"
+// ! cant change the value of a constant variable
+// username="bhatia" // This will throw an error
+console.log(username)
+// username="bhatia" // This will throw an error
+// console.log(username)
