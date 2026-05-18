106be8edd97cec2b9d9c1f043b903d00d1c81115
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-18 15:53:20 +0530

    add score and num1 calculations with shorthand operators in JavaScript

---
+++ JavaScript/part1/assignment.js
@@ -0,0 +1,9 @@
+let score = 10+11;
+let num1=10;
+
+num1=num1+5;
+num1+=5; // This is a shorthand for num1 = num1 + 5
+num1-=3; // This is a shorthand for num1 = num1 - 3
+
+console.log("Score:", score);
+console.log("Num1 after addition:", num1);
