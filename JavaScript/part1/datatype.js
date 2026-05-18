34f16a4adf696b61f2a9628cdd0b44ca89a06cc9
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-18 15:31:53 +0530

    datatypes in java script

---
+++ JavaScript/part1/datatype.js
@@ -0,0 +1,36 @@
+let name = "Alice";
+
+/*
+!Data types in JavaScript:
+~1. Primitive Types:
+    ^- String: Represents textual data. Example: "Hello, World!"
+    ^- Number: Represents numeric values. Example: 42, 3.14
+    ^- Boolean: Represents true or false values. Example: true, false
+    ^- Null: Represents the intentional absence of any object value. Example: null
+    ^- Undefined: Represents a variable that has been declared but not assigned a value. Example: undefined
+~ 2. Non-Primitive Types:
+    ^- Object: Represents a collection of properties. Example: { name: "Alice", age: 30 }
+    ^- Array: Represents an ordered list of values. Example: [1, 2, 3, 4, 5]
+    ^- Function: Represents a reusable block of code. Example: function greet() { console.log("Hello!"); }
+
+    ^- Symbol: Represents a unique identifier. Example: Symbol("id")
+
+*/
+// ! var - 
+var score=102;
+// ! let -
+// int
+let age=30;
+// string
+let name1="Alice";
+// boolean
+let isStudent=true;
+
+// ^ object
+let teaTypes={"tea":"lemon tea","green tea":"green tea","black tea":"black tea"};
+// ^ array
+let numbers=[1,2,3,4,5]
+
+let getScore=score
+
+console.log("Score:", getScore);
