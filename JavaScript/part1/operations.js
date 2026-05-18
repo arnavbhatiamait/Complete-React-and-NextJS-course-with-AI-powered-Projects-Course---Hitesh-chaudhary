2d11b9037dc6d0f47e196aac6b68bbc149e3e1e6
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-18 15:48:14 +0530

    operations in js

---
+++ JavaScript/part1/operations.js
@@ -0,0 +1,47 @@
+let addition=4+5;
+
+let score =102;
+let bonus = 25;
+let totalScore=score+bonus;
+
+let addition1=10+20;
+let subtraction=10-5;
+let multiplication=10*5;
+let division=10/2;
+
+let remainder=10%3;
+let exponentiation=2**3;
+
+// ! increment and decrement
+let count=0;
+count++; // Incrementing the count by 1
+console.log("Count after incrementing:", count);
+count--; // Decrementing the count by 1
+console.log("Count after decrementing:", count);
+++count; // Incrementing the count by 1 again
+console.log("Count after incrementing again:", count);
+--count; // Decrementing the count by 1 again
+console.log("Count after decrementing again:", count);
+
+console.log("Addition:", addition);
+console.log("Total Score:", totalScore);
+console.log("Addition:", addition1);
+console.log("Subtraction:", subtraction);
+console.log("Multiplication:", multiplication);
+console.log("Division:", division);
+console.log("Remainder:", remainder);
+console.log("Exponentiation:", exponentiation);
+
+
+// ! comparison operators
+
+let a=10;
+let b=20;
+console.log("Is a equal to b?", a==b);
+console.log("Is a not equal to b?", a!=b);
+console.log("Is a greater than b?", a>b);
+console.log("Is a less than b?", a<b);
+console.log("Is a greater than or equal to b?", a>=b);
+console.log("Is a less than or equal to b?", a<=b);
+console.log("Is a strictly equal to b?", a===b);
+console.log("Is a strictly not equal to b?", a!==b);
