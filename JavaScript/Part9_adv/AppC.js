52b7acfd68db1886e3a56f5d2523e4a2b669729e
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-19 16:23:41 +0530

    common js modules export

---
+++ JavaScript/Part9_adv/AppC.js
@@ -0,0 +1,6 @@
+const mathOperations=require('./mathOperationsC.js')
+
+console.log(mathOperations.multiply(3, 4)); // Output: 12
+console.log(mathOperations.add(5, 7));
+console.log(mathOperations.subtract(10, 3));
+console.log(mathOperations.divide(10, 2));
