6b60bdbe0deca60ec31eb778ac7bd1ccc8ee1862
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-19 19:06:12 +0530

    added local storage functionality in to do list

--- JavaScript/Projects/01_todo_with_local_storage/script.js
+++ JavaScript/Projects/01_todo_with_local_storage/script.js
@@ -1,24 +1,34 @@
-let todoInput=document.getElementById('todo-input');
-let addButton=document.getElementById('add-task-btn');
-let todoList=document.getElementById('todo-list');
-let tasks=[]
 
 document.addEventListener("DOMContentLoaded", ()=>{
     console.log("DOM fully loaded and parsed");
+    let todoInput=document.getElementById('todo-input');
+    let addButton=document.getElementById('add-task-btn');
+    let todoList=document.getElementById('todo-list');
+    let tasks=localStorage.getItem("tasks") ? JSON.parse(localStorage.getItem("tasks")) : [];
+
+    tasks.forEach(element => {
+        RenderTasks(element);
+    });
 
     addButton.addEventListener(
         'click', ()=>{
-            const taskText=todoInput.ariaValueMax.trim();
+            const taskText=todoInput.value.trim();
             if(taskText==="") return;
             const newTask={
                 id:Date.now(),
                 text:taskText,
                 completed:false }
             tasks.push(newTask);
-            RenderTasks();
+            RenderTasks(newTask);
+            saveTasks(tasks);
             todoInput.value="";
             console.log(tasks);
         }
-    )
-    
+    );
+    function saveTasks(){
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    };
+    function RenderTasks(tasks){
+
+    }   
 })
