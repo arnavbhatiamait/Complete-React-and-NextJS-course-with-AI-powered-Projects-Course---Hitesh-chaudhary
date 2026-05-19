adbccd6029d318d1acf675634c9a067e31d4c58a
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-19 16:20:50 +0530

    importing modules

---
+++ JavaScript/Part9_adv/AppM.js
@@ -0,0 +1,10 @@
+import {multiply}  from "./mathOperationsM.js";
+import {add} from './mathOperationsM.js';
+
+// ! named import
+import { subtract,divide} from "./mathOperationsM.js";
+
+console.log(multiply(3, 4)); // Output: 12
+console.log(add(5, 7)); // Output: 12
+console.log(subtract(10, 3)); // Output: 7
+console.log(divide(10, 2)); // Output: 5
