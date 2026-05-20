8328e12e84c403e13e49b55b78100fdc33e33d39
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-21 02:08:02 +0530

    not found page implemented

---
+++ Nextjs/module3/src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+const c = () => {
+  return (
+    <div>
+     <h1>404</h1> Not found Cusotm
+      <Link href="/">Go to Home</Link>
+    </div>
+  )
+}
+
+export default c
