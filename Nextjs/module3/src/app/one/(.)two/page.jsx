cad7656a782383eeb18ee707353575024b0cb7ef
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-21 00:54:48 +0530

    Same Level Intercepted Route

---
+++ Nextjs/module3/src/app/one/(.)two/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Same level 2 page
+    </div>
+  )
+}
+
+export default page
---
+++ Nextjs/module3/src/app/one/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+function page() {
+  return (
+    <div>
+      <Link href="/one/two">Go to Two</Link>
+    </div>
+  )
+}
+
+export default page
