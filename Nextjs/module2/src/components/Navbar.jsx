0b5ce67c65d7976ad29f9a1b8d9f88c5f4be3482
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-20 18:06:11 +0530

    added navigation to different pages in module 2

--- Nextjs/module2/src/components/Navbar.jsx
+++ Nextjs/module2/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 export function Navbar() {
   return    (
@@ -11,10 +12,19 @@ export function Navbar() {
       <span className="ml-3 text-xl">Tailblocks</span>
     </a>
     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a className="mr-5 hover:text-white">First Link</a>
-      <a className="mr-5 hover:text-white">Second Link</a>
-      <a className="mr-5 hover:text-white">Third Link</a>
-      <a className="mr-5 hover:text-white">Fourth Link</a>
+      <Link href="/" className="mr-5 hover:text-white">
+        Home
+      </Link>
+      <Link href="/about" className="mr-5 hover:text-white">
+        About
+      </Link>
+      <Link href="/user" className="mr-5 hover:text-white">
+        User
+      </Link>
+      <Link href="/Contact" className="mr-5 hover:text-white">
+        Contact
+      </Link>
+   
     </nav>
     <button className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">Button
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
