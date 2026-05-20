d319ae42187f3896914dc6bf64dfb602c175cbb2
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-21 01:13:19 +0530

    2 level above route service

---
+++ Nextjs/module3/src/app/one/two/(..)(..)four/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      Intecepted 4 page
+    </div>
+  )
+}
+
+export default page
--- Nextjs/module3/src/app/one/two/page.jsx
+++ Nextjs/module3/src/app/one/two/page.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import Link from 'next/link'
 const page = () => {
   return (
     <div>
-      two
+     <h1>Two</h1>
+     <div >
+      <Link href="/four">four page</Link>
+     </div>
     </div>
   )
 }
