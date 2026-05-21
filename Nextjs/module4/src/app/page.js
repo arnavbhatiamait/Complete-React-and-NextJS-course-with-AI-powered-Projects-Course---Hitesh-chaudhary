8505104085fdc2759dd5f45f2d74591b8d7c6c8c
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-22 00:55:44 +0530

    "use client" -> added client component in the file

--- Nextjs/module4/src/app/page.js
+++ Nextjs/module4/src/app/page.js
@@ -1,13 +1,26 @@
 // import Image from "next/image";
-import React from "react";
-export default async function HomePage() {
-  const data = await fetch("https://jsonplaceholder.typicode.com/todos")
-  const jsonData = await data.json()
+// import React from "react";
+// export default async function HomePage() {
+//   const data = await fetch("https://jsonplaceholder.typicode.com/todos")
+//   const jsonData = await data.json()
+//   return (
+//     <div>
+//       <h1>Home Page</h1>
+//       <p>{JSON.stringify(jsonData)}</p>
+//     </div>
+    
+//   );
+// }
+"use client"
+import React from "react"
+const HomePage = () => {
+  const [name, setName] = React.useState("John Doe")
   return (
     <div>
       <h1>Home Page</h1>
-      <p>{JSON.stringify(jsonData)}</p>
+      <p>Name: {name}</p>
+      <button onClick={() => setName("AABC")}>Change Name</button>
     </div>
-    
-  );
+  )
 }
+export default HomePage
