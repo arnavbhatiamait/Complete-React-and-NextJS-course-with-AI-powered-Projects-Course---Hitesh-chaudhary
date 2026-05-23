07b047be771948f80219bf309343435c221b59d0
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-23 20:07:23 +0530

    traditional way to get cookies from headers in next js

--- Nextjs/module5/src/app/api/user/route.js
+++ Nextjs/module5/src/app/api/user/route.js
@@ -12,12 +12,16 @@ import {headers} from "next/headers";
 // ^ method 2
 export async function GET(request) {
     const reqHeaders = await headers();
+    // ! traditional way to get cookies from headers
+    const username= request.cookies.get("username");
+    console.log(username);
     console.log(reqHeaders.get("Authorization"));
     // return Response.json({ message: "Hello, Next.js!" });.
     // & cusom header
     return new Response("<h1>Hello, Next.js!</h1>", {
         headers: {
             "Content-Type": "text/html",
+            "set-cookie": "username=Arnav"
         }
     });
 }
