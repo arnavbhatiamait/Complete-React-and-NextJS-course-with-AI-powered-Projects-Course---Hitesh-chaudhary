7408f7f5a92437829146bb82af113e643a6b5afe
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-23 20:09:49 +0530

    modern cookie way

--- Nextjs/module5/src/app/api/user/route.js
+++ Nextjs/module5/src/app/api/user/route.js
@@ -1,5 +1,5 @@
 // ! headers support in nextjs 
-import {headers} from "next/headers";
+import {headers, cookies} from "next/headers";
 // ^ method 1
 // export async function GET(request) {
 
@@ -13,8 +13,15 @@ import {headers} from "next/headers";
 export async function GET(request) {
     const reqHeaders = await headers();
     // ! traditional way to get cookies from headers
-    const username= request.cookies.get("username");
+    // const username= request.cookies.get("username");
+    // console.log(username);
+
+    const cookieStore = await cookies();
+    const username = cookieStore.get("username");
     console.log(username);
+
+    cookieStore.set("theme", "Dark");
+    console.log(cookieStore);
     console.log(reqHeaders.get("Authorization"));
     // return Response.json({ message: "Hello, Next.js!" });.
     // & cusom header
