12547d54af403cccee55326dd57ab76916622d7b
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-25 01:57:41 +0530

    use router hook in next js to navigate bw paths

--- Nextjs/module6/src/app/shop/orders/page.jsx
+++ Nextjs/module6/src/app/shop/orders/page.jsx
@@ -1,10 +1,15 @@
+"use client"
 import React from 'react'
-
+import {useRouter} from 'next/navigation'
 const orders = () => {
+    const router=useRouter();
   return (
-    <div>
-      
+    <>
+    <div className='hover:cursor-pointer' onClick={()=>router.push('/shop/products')}>
+        Go to Products
     </div>
+        <button onClick={(e)=>{router.refresh()}}>Refresh</button>
+    </>
   )
 }
 
--- Nextjs/module6/src/app/shop/products/page.jsx
+++ Nextjs/module6/src/app/shop/products/page.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const products = () => {
   return (
     <div>
+         <h1>Products Page</h1>
       
     </div>
   )
