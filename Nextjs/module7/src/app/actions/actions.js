912d136a4dd7d17aaf73b48d1d03c4d0deae4b09
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-25 22:45:17 +0530

    add server actions and update page component for todo functionality

--- Nextjs/module7/src/app/actions/actions.js
+++ Nextjs/module7/src/app/actions/actions.js
@@ -1,5 +1,18 @@
 "use server"
-export async function createToDo(formData){
+export async function createToDo(formData) {
     const title = formData.get("title")
     console.log(title)
+}
+
+export async function updateToDo(title, desc, isCompleted) {
+    const newData = {
+        title,
+        desc,
+        isCompleted
+    }
+    console.log(newData)
+    return {
+        success: true,
+        data: newData
+    }
 }
