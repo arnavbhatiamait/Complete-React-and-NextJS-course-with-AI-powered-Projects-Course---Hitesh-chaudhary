6472698d213d56e2b09d37a4130da651d2e4ed2f
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-05-30 14:20:55 +0530

    absolute title

--- Nextjs/module8/src/app/dashboard/settings/page.jsx
+++ Nextjs/module8/src/app/dashboard/settings/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 export const metadata = {
-  title: "Settings Page",
-  description: "This is the settings page of the application",
+//   title: "Settings Page",
+//   description: "This is the settings page of the application",
+    title: {
+        absolute: "Settings Page",
+    },
+    description: "This is the settings page of the application",
 };
 
 const settings = () => {
