126c9516904edf798d436fc14022ff33baebd254
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-02 01:52:42 +0530

    fix(auth): update import for useRouter from next/navigation in LogoutButton component feat(auth): enable email and password authentication in auth configuration

--- Nextjs/module11/components/logout-button.tsx
+++ Nextjs/module11/components/logout-button.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import { Button } from './ui/button'
 import { authClient } from '@/lib/auth-client'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { toast } from 'sonner'
 const LogoutButton = () => {
     const router = useRouter();
