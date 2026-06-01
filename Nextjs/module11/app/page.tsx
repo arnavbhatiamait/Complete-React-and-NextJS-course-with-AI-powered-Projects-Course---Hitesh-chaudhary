1961b93ab9d7d95af571331055f3f9f65f618116
arnavbhatiamait <arnavbhatiamait@gmail.com>
2026-06-02 00:50:58 +0530

    feat(auth): implement authentication guard and enhance login functionality with social providers

--- Nextjs/module11/app/page.tsx
+++ Nextjs/module11/app/page.tsx
@@ -1,14 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { requiredAuth } from "@/lib/auth-guard";
+import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="flex justify-center bg-zinc-900 items-center h-screen" >
-      <Button 
-      variant="destructive"
-      size='lg'
-      >
-        Click Me
-      </Button>
-    </div>
-  );
-}
+async function Home() {
+  const session = await requiredAuth();
+  const {user} = session;
+
+    return (
+      <div className="flex justify-center bg-zinc-900 items-center h-screen" >
+        <Image src={user.image!} alt="Profile Picture" width={100} height={100} className="rounded-full mb-4" />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        
+      </div>
+    );
+  }
+export default Home
---
+++ Nextjs/module11/lib/auth-guard.ts
@@ -0,0 +1,24 @@
+import {auth} from "@/lib/auth";
+import {headers} from "next/headers";
+import {redirect} from "next/navigation";
+
+export async function requiredAuth(){
+
+    const session=await auth.api.getSession({
+        headers: await headers()
+    });
+    if(!session){
+        redirect("/login");
+    }
+    return session;
+}
+
+export async function requireUnAuth(){
+    const session=await auth.api.getSession({
+        headers: await headers()
+    });
+    if(session){
+        redirect("/");
+    }
+    return session;
+}
